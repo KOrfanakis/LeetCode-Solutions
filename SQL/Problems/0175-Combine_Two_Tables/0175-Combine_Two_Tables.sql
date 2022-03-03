8e021bf8ca94a09097ecb62d8383ceb195e5eb87
Konstantinos Orfanakis <73746797+KOrfanakis@users.noreply.github.com>
2022-03-03 08:16:54 +0000

    Uploaded sql file (0175)

---
+++ SQL/Problems/0175-Combine_Two_Tables/0175-Combine_Two_Tables.sql
@@ -0,0 +1,9 @@
+/*
+LeetCode 175. Combine Two Tables
+https://leetcode.com/problems/combine-two-tables/
+*/
+
+SELECT firstName, lastName, city, state
+FROM Person
+    LEFT JOIN Address
+    ON Person.personId = Address.personId;
