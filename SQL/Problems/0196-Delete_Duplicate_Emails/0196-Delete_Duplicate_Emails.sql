55d248ed3e08a94fe1410c5b8235bf153c963e37
Konstantinos Orfanakis <73746797+KOrfanakis@users.noreply.github.com>
2022-03-21 14:54:10 +0000

    Upload sql file (0196)

---
+++ SQL/Problems/0196-Delete_Duplicate_Emails/0196-Delete_Duplicate_Emails.sql
@@ -0,0 +1,12 @@
+/*
+LeetCode 196. Delete Duplicate Emails
+https://leetcode.com/problems/delete-duplicate-emails/
+*/
+
+
+DELETE p1 
+FROM 
+    Person p1,
+    Person p2
+WHERE
+    p1.email = p2.email AND p1.id > p2.id;
